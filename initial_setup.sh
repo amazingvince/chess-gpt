eac596cf07b3b14e5a4cf80809e779c8bf00f85b
Kion <fallah.kion@gmail.com>
2024-12-15 19:32:12 +0000

    Add Flash Attention at Setup Time

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 apt update && apt install -y vim tmux
 curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+# We need to build the packages without flash-attn first, and then build flash-attn with dependencies
+uv sync --no-install-package flash-attn
 uv sync --no-build-isolation
 source .venv/bin/activate
+wandb login
+huggingface-cli login
+accelerate config
